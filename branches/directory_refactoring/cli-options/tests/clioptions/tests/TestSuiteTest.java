a911cd2939f1b2b42c865cdf3c89770ae25e8e28
hindin@gmail.com <hindin@gmail.com@cfe0e4d0-763e-4282-546f-8037eb184a69>
2011-04-24 20:23:27 +0000

    netbeans support

---
+++ branches/directory_refactoring/cli-options/tests/clioptions/tests/TestSuiteTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package clioptions.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author jhindin
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({clioptions.tests.TestArrayParsingLong.class,clioptions.tests.TestListParsingLong.class,clioptions.tests.TestCreationAndParsingShort.class,clioptions.tests.TestCreation.class,clioptions.tests.TestParsingShort.class,clioptions.tests.TestCreationAndParsingLong.class})
+public class TestSuiteTest {
+
+}
